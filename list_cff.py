0a444b0e25d3a7c8f08cba03f0702a528bb53e17
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2018-02-06 00:40:15 +0100

    Added some of the steamflow code

---
+++ list_cff.py
@@ -0,0 +1,5 @@
+inputFileNames = [
+'root://eoscms.cern.ch//eos/cms/tier0/store/data/Run2017E/EphemeralHLTPhysics1/RAW/v1/000/304/777/00000/DC6DA840-B8AD-E711-9D24-02163E01A4AD.root',
+'root://eoscms.cern.ch//eos/cms/tier0/store/data/Run2017E/EphemeralHLTPhysics1/RAW/v1/000/304/777/00000/3484B2EC-BBAD-E711-9C4E-02163E012257.root',
+'root://eoscms.cern.ch//eos/cms/tier0/store/data/Run2017E/EphemeralHLTPhysics1/RAW/v1/000/304/777/00000/F8308F22-BBAD-E711-837E-02163E01A40F.root'
+]
