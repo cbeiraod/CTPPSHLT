cbd4813d733c594c92bc89ee8a6070cdf3f66125
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2018-02-06 11:17:48 +0000

    Updated information for rates

--- Rates/Menu_HLT.py
+++ Rates/Menu_HLT.py
@@ -1,47 +1,80 @@
 groupMap = {
     'HLT_PFJet500_v': ['Physics'],
     'HLT_CTPPSCoincidence_v':  ['PhysicsUser','PhysicsUserControl'],
-    'HLT_CTPPSCoincidenceL1_v': ['PhysicsUser','PhysicsUserControl'],
-    'HLT_DiPFJetAve260_v': ['PhysicsUser','PhysicsUserControl'],
+    'HLT_CTPPSCoincidenceL1Single_v': ['PhysicsUser','PhysicsUserControl'],
+    'HLT_CTPPSCoincidenceL1Double_v': ['PhysicsUser','PhysicsUserControl'],
+
+    'HLT_DiPFJetAve260_v': ['PhysicsUser','PhysicsUserUserSignal'],
     'HLT_CTPPS_DiPFJetAve260_v': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_DiPFJetAve240_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_CTPPS_DiPFJetAve240_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_DiPFJetAve220_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_CTPPS_DiPFJetAve220_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_DiPFJetAve200_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_CTPPS_DiPFJetAve200_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_DiPFJetAve180_test': ['PhysicsUser','PhysicsUserSignal'],
-    'HLT_CTPPS_DiPFJetAve180_test': ['PhysicsUser','PhysicsUserSignal']
+    'HLT_CTPPS_DiPFJetAve260_Double_v': ['PhysicsUser','PhysicsUserSignal'],
+
+    'HLT_DiPFJetAve240_v': ['PhysicsUser','PhysicsUserUserSignal'],
+    'HLT_CTPPS_DiPFJetAve240_v': ['PhysicsUser','PhysicsUserSignal'],
+    'HLT_CTPPS_DiPFJetAve240_Double_v': ['PhysicsUser','PhysicsUserSignal'],
+
+    'HLT_DiPFJetAve220_v': ['PhysicsUser','PhysicsUserUserSignal'],
+    'HLT_CTPPS_DiPFJetAve220_v': ['PhysicsUser','PhysicsUserSignal'],
+    'HLT_CTPPS_DiPFJetAve220_Double_v': ['PhysicsUser','PhysicsUserSignal'],
+
+    'HLT_DiPFJetAve200_v': ['PhysicsUser','PhysicsUserUserSignal'],
+    'HLT_CTPPS_DiPFJetAve200_v': ['PhysicsUser','PhysicsUserSignal'],
+    'HLT_CTPPS_DiPFJetAve200_Double_v': ['PhysicsUser','PhysicsUserSignal'],
+
+    'HLT_DiPFJetAve180_v': ['PhysicsUser','PhysicsUserUserSignal'],
+    'HLT_CTPPS_DiPFJetAve180_v': ['PhysicsUser','PhysicsUserSignal'],
+    'HLT_CTPPS_DiPFJetAve180_Double_v': ['PhysicsUser','PhysicsUserSignal'],
     }
 
 datasetMap = {
     'HLT_PFJet500_v': ['Physics'],
     'HLT_CTPPSCoincidence_v': ['PhysicsUser'],
-    'HLT_CTPPSCoincidenceL1_v': ['PhysicsUser'],
+    'HLT_CTPPSCoincidenceL1Single_v': ['PhysicsUser'],
+    'HLT_CTPPSCoincidenceL1Double_v': ['PhysicsUser'],
+
     'HLT_DiPFJetAve260_v': ['PhysicsUser'],
     'HLT_CTPPS_DiPFJetAve260_v': ['PhysicsUser'],
-    'HLT_DiPFJetAve240_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve240_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve220_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve220_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve200_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve200_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve180_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve180_test': ['PhysicsUser']
+    'HLT_CTPPS_DiPFJetAve260_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve240_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve240_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve240_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve220_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve220_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve220_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve200_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve200_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve200_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve180_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve180_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve180_Double_v': ['PhysicsUser'],
     }
 
 streamMap = {
     'HLT_PFJet500_v': ['Physics'],
     'HLT_CTPPSCoincidence_v': ['PhysicsUser', 'MinBias'],
-    'HLT_CTPPSCoincidenceL1_v': ['PhysicsUser', 'MinBias'],
+    'HLT_CTPPSCoincidenceL1Single_v': ['PhysicsUser', 'MinBias'],
+    'HLT_CTPPSCoincidenceL1Double_v': ['PhysicsUser', 'MinBias'],
+
     'HLT_DiPFJetAve260_v': ['PhysicsUser'],
     'HLT_CTPPS_DiPFJetAve260_v': ['PhysicsUser'],
-    'HLT_DiPFJetAve240_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve240_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve220_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve220_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve200_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve200_test': ['PhysicsUser'],
-    'HLT_DiPFJetAve180_test': ['PhysicsUser'],
-    'HLT_CTPPS_DiPFJetAve180_test': ['PhysicsUser']
+    'HLT_CTPPS_DiPFJetAve260_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve240_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve240_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve240_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve220_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve220_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve220_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve200_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve200_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve200_Double_v': ['PhysicsUser'],
+
+    'HLT_DiPFJetAve180_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve180_v': ['PhysicsUser'],
+    'HLT_CTPPS_DiPFJetAve180_Double_v': ['PhysicsUser'],
     }
--- Rates/filesInput.py
+++ Rates/filesInput.py
@@ -1,3 +1,8 @@
 fileInputNames = [
-"../Prod/hlt.root"
+"/eos/user/c/cbeiraod/CT-PPS/HLT/base/allTracks/hlt.root",
+"/eos/user/c/cbeiraod/CT-PPS/HLT/double/allTracks/hlt.root",
+"/eos/user/c/cbeiraod/CT-PPS/HLT/longer1/allTracks/hlt.root",
+"/eos/user/c/cbeiraod/CT-PPS/HLT/longer2/allTracks/hlt.root",
+#"/eos/user/c/cbeiraod/CT-PPS/HLT/longer3/allTracks/hlt.root",
+#"/eos/user/c/cbeiraod/CT-PPS/HLT/longer4/allTracks/hlt.root",
 ]
