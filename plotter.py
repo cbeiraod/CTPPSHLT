b58c8dd4b597b4001712cc8a6336154b0924eb13
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2018-03-02 17:19:57 +0000

    Added all variables I am interested in

--- plotter.py
+++ plotter.py
@@ -30,15 +30,101 @@ if __name__ == "__main__":
   }
 
   plots = {
-    "Arm1Xi": {
+    "Arm1_Xi": {
       "name": "h_arm1xi",
       "logy": False,
       "logz": False,
+      "opt": "",
     },
-    "Arm2Xi": {
+    "Arm2_Xi": {
       "name": "h_arm2xi",
       "logy": False,
       "logz": False,
+      "opt": "",
+    },
+    "Proton_Mass": {
+      "name": "h_protonMass",
+      "logy": False,
+      "logz": False,
+      "opt": "",
+    },
+    "Proton_Rapidity": {
+      "name": "h_protonRapidity",
+      "logy": False,
+      "logz": False,
+      "opt": "",
+    },
+    "Proton_MassVRapidity": {
+      "name": "h_proton_massVSrapidity",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "Dijet_Arm1_Xi": {
+      "name": "h_arm1dijetxi",
+      "logy": True,
+      "logz": False,
+      "opt": "",
+    },
+    "Dijet_Arm2_Xi": {
+      "name": "h_arm2dijetxi",
+      "logy": True,
+      "logz": False,
+      "opt": "",
+    },
+    "Dijet_Mass": {
+      "name": "h_dijetMass",
+      "logy": False,
+      "logz": False,
+      "opt": "",
+    },
+    "Dijet_Rapidity": {
+      "name": "h_dijetRapidity",
+      "logy": False,
+      "logz": False,
+      "opt": "",
+    },
+    "Dijet_MassVRapidity": {
+      "name": "h_dijet_massVSrapidity",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "Arm1_Xi_DijetVProton": {
+      "name": "h_arm1vsdijetxi_matched",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "Arm2_Xi_DijetVProton": {
+      "name": "h_arm2vsdijetxi_matched",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "Mass_DijetVProton": {
+      "name": "h_massVSdijet",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "Rapidity_DijetVProton": {
+      "name": "h_rapidityVSdijet",
+      "logy": False,
+      "logz": False,
+      "opt": "colz",
+    },
+    "DijetMassFraction": {
+      "name": "h_dijetMassFraction",
+      "logy": False,
+      "logz": False,
+      "opt": "",
+    },
+    "DijetRapidityDifference": {
+      "name": "h_dijetRapidityDifference",
+      "logy": False,
+      "logz": False,
+      "opt": "",
     },
   }
 
@@ -63,7 +149,11 @@ if __name__ == "__main__":
         canvas.SetLogy()
       if plots[plotName]["logz"]:
         canvas.SetLogz()
-      hist.Draw()
+
+      if plots[plotName]["opt"] is not "":
+        hist.Draw(plots[plotName]["opt"])
+      else:
+        hist.Draw()
 
       canvas.Update()
       canvas.SaveAs(args.outDirectory + "/" + condition + "_" + plotName + ".png")
